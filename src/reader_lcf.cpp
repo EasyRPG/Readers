5b45d9fce1d8d9a64a7e49e8ca11dfc692bbc2ea
Ghabry <gabriel+github@mastergk.de>
2021-03-15 17:25:14 +0100

    WIP: Add lcf logger

--- src/reader_lcf.cpp
+++ src/reader_lcf.cpp
@@ -12,6 +12,7 @@
 #include <istream>
 
 #include "lcf/reader_lcf.h"
+#include "log.h"
 
 namespace lcf {
 // Statics
@@ -259,7 +260,7 @@ int LcfReader::Peek() {
 }
 
 void LcfReader::Skip(const struct LcfReader::Chunk& chunk_info, const char* where) {
-	fprintf(stderr, "Skipped Chunk %02X (%" PRIu32 " byte) in lcf at %" PRIX32 " (%s)\n",
+	Log::Debug("Skipped Chunk %02X (%" PRIu32 " byte) in lcf at %" PRIX32 " (%s)\n",
 			chunk_info.ID, chunk_info.length, Tell(), where);
 
 	for (uint32_t i = 0; i < chunk_info.length; ++i) {
